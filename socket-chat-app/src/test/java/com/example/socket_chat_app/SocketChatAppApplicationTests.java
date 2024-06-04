d30058ed9f1ddea9408e83422620b0075fedcf4c
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-05 01:25:56 +0300

    Init

---
+++ socket-chat-app/src/test/java/com/example/socket_chat_app/SocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socket_chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
