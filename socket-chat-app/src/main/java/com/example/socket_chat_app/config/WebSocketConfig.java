62c059769fb8405711ae1e30a8e2d6ce932e2086
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-05 01:55:55 +0300

    Create WebSocketConfig

---
+++ socket-chat-app/src/main/java/com/example/socket_chat_app/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.example.socket_chat_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+//WebSocketMessageBrokerConfigurer: Interface for connect with server to establish connection.
+//@EnableWebSocketMessageBroker: Annotation to allow WS implement to WebSocketMessageBrokerConfigurer.
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // registerStompEndpoints to select URL to connect with server.
+    // STOMP: Streaming Text Messaging Protocol.
+    // STOMP: exchange and transfer data.
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/connect").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    /* Note: WebSocket just open connection between client side and server side
+             WebSocket can not define roles or users or transfer data
+     */
+}
