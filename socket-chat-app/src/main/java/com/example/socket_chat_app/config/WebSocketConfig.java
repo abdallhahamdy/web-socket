c58a2cd0184abfeda4dc83245a321c1fb2852101
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-06 05:05:46 +0300

    Create ChatController

--- socket-chat-app/src/main/java/com/example/socket_chat_app/config/WebSocketConfig.java
+++ socket-chat-app/src/main/java/com/example/socket_chat_app/config/WebSocketConfig.java
@@ -12,12 +12,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 //@EnableWebSocketMessageBroker: Annotation to allow WS implement to WebSocketMessageBrokerConfigurer.
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    // registerStompEndpoints to select URL to connect with server.
+    // registerStompEndpoints to select URL to connect wi th server.
     // STOMP: Streaming Text Messaging Protocol.
     // STOMP: exchange and transfer data.
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect").withSockJS();
+        registry.addEndpoint("/connect").withSockJS(); // In front side
     }
 
     @Override
---
+++ socket-chat-app/src/main/java/com/example/socket_chat_app/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.example.socket_chat_app.controller;
+
+import com.example.socket_chat_app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    // SimpMessageHeaderAccessor: For Adding Users whose login for first time
+    // Payload like body
+    @MessageMapping("/chat.login")
+    @SendTo("/topic")
+    public ChatMessage login(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic")
+    public ChatMessage send(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+}
