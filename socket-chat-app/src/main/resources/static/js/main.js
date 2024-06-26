ae4bf47876f1758745bfcbf4eaf84eefbdba120e
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2024-06-26 23:11:00 +0300

    Create send Message function

--- socket-chat-app/src/main/resources/static/js/main.js
+++ socket-chat-app/src/main/resources/static/js/main.js
@@ -4,6 +4,7 @@ var chatElement = document.querySelector('#chat');
 var userForm = document.querySelector('#userForm');
 var connect = document.querySelector('#connect');
 var mainChat = document.querySelector('#main-chat');
+var sendDiv = document.querySelector('#sendDiv');
 var userName = null;
 var stomp = null;
 var URL = "http://localhost:8080";
@@ -38,6 +39,8 @@ function sendMessage(payload){
         joinUser(message, "join")
     } else if (message.chatType == 'LEAVE') {
         joinUser(message, "leave")
+    } else {
+        console.log("*************** Chat")
     }
 }
 function joinUser(message, state){
@@ -53,4 +56,17 @@ function joinUser(message, state){
     li2.appendChild(hr2)
     mainChat.appendChild(li2);
 }
+function send() {
+    var messageUser = document.querySelector('#sms').value.trim();
+    if(messageUser && stomp){
+        var userMessage = {
+            message: messageUser,
+            chatType: 'CHAT',
+            sender: userName
+        }
+        stomp.send("/app/chat.send",{}, JSON.stringify(userMessage))
+        document.querySelector('#sms').value = '';
+    }
+}
 userForm.addEventListener('submit', connectSocket)
+sendDiv.addEventListener('click', send)
